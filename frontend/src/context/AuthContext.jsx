982422bdc8b06a10078bfe282e43f5373333660d
belhaddadMohamed <belhaddad.mohamed7@gmail.com>
2024-02-01 17:50:58 +0100

    Add logout view

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,11 +1,11 @@
-import React, { createContext } from 'react' 
+import React, { createContext, useEffect } from 'react' 
 import { useHistory } from 'react-router-dom'
 import jwt_decode from jwt_decode
 
 const AuthContext = createContext()
 export default AuthContext;
 
-export const AuthProvider = () => {
+export const AuthProvider = ({children}) => {
 
     ////////////// VARIABLES //////////////
 
@@ -53,8 +53,9 @@ export const AuthProvider = () => {
         }
     }
 
+
     const registerUser = async (username, email, password, password2) => {
-        const response = await fetch('127.0.0.1:8000/api/register/', {
+        const response = await fetch('http://127.0.0.1:8000/api/register/', {
             method: 'POST',
             headers: {
                 'content-type': 'application/json',
@@ -68,7 +69,34 @@ export const AuthProvider = () => {
 
         if(data.status === 201){
             console.log("New user has been created!")
+            history.push('/login')
         }
     }
 
+
+    const logoutUser = () => {
+        setAuthToken(null)
+        setUser(null)
+        localStorage.removeItem('authToken')
+        history.push('/login')
+    }
+
+
+    context_data = {
+        user,
+        setUser,
+        authToken,
+        setAuthToken,
+        loginUser,
+        registerUser,
+        logoutUser,
+    }
+
+
+    useEffect(()=>{
+        response = await fetch('http://127.0.0.1:8000/token/refresh/')
+    },[loading])
+
+
+    
 }
